39e268896fe64635e9e76231a73d3336e8b2b86e
pranav <prnvkatkar88@gmail.com>
2021-11-11 13:03:56 +0530

    Refactored:test case enhanced invoice

--- InvoiceServiceTest.java
+++ InvoiceServiceTest.java
@@ -1,14 +1,21 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 import junit.framework.Assert;
 
-class InvoiceServiceTest {
+	class InvoiceServiceTest {
+	InvoiceGenerator invoiceGenerator = null;
 
-	@Test
+	@Before
+	public void setUp() throws Exception {
+		invoiceGenerator = new InvoiceGenerator();
+
+	}
+
+//	@Test
 	public void givenDistanceAndTime_ShoildReturnTotalFare() {
-		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
 		double distance = 2.0;
 		int time = 5;
 		double fare = invoiceGenerator.calculateFare(distance, time);
@@ -16,25 +23,31 @@ class InvoiceServiceTest {
 
 	}
 
-	@Test
+//	@Test
 	public void givenLessDistanceOrTime_ShouldReturnMinFare() {
-		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
 		double distance = 0.1;
 		int time = 1;
 		double fare = invoiceGenerator.calculateFare(distance, time);
 		Assert.assertEquals(5, fare, 0.0);
 
 	}
-	
-	@Test
+
+	//@Test
 	public void givenMultipleRides_ShouldReturnTotalFare() {
-		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
 		Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
 		double fare = invoiceGenerator.calculateFare(rides);
 		Assert.assertEquals(30, fare, 0.0);
 
 	}
-
 	
+	@Test
+	public void givenMultipleRides_ShouldReturnInvoiceSummary() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+		InvoiceSummary summary = invoiceGenerator.calculateFare(rides);
+		InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2,30);
+		Assert.assertEquals(expectedInvoiceSummary, summary);
+
+	}
 
 }
