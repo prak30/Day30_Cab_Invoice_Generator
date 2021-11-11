fdcf85bf33481a1f60a9e35db9134e89437e7386
pranav <prnvkatkar88@gmail.com>
2021-11-11 09:43:49 +0530

    Added:calculates total fare

--- InvoiceGenerator.java
+++ InvoiceGenerator.java
@@ -1,7 +1,15 @@
 
 public class InvoiceGenerator {
 
-	public static void main(String[] args) {
-		System.out.println("WELCOME TO INVOICE GENERATOR");
+	private static final int COST_PER_TIME = 1;
+	private static final double MINIMUM_COST_PER_KILOMETER = 10;
+	private static final double MINIMUM_FARE = 5;
+
+	public double calculateFare(double distance, int time) {
+		double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+		if (totalFare < MINIMUM_FARE)
+			return MINIMUM_FARE;
+		return totalFare;
 	}
+
 }
