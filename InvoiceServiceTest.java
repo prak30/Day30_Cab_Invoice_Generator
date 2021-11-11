5f285bc93146e39ec1ea797be2452b8a6a674dfd
pranav <prnvkatkar88@gmail.com>
2021-11-11 09:45:07 +0530

    Added:test case for total fare

---
+++ InvoiceServiceTest.java
@@ -0,0 +1,31 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import junit.framework.Assert;
+
+class InvoiceServiceTest {
+
+	@Test
+	public void givenDistanceAndTime_ShoildReturnTotalFare() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		double distance = 2.0;
+		int time = 5;
+		double fare = invoiceGenerator.calculateFare(distance, time);
+		Assert.assertEquals(25, fare, 0.0);
+
+	}
+
+	@Test
+	public void givenLessDistanceOrTime_ShouldReturnMinFare() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		double distance = 0.1;
+		int time = 1;
+		double fare = invoiceGenerator.calculateFare(distance, time);
+		Assert.assertEquals(5, fare, 0.0);
+
+	}
+
+	
+
+}
